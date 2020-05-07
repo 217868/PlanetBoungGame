1988cd53222ea9a593ca370673178b7cc9dbd058
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2020-05-08 00:18:07 +0100

    Ship logic and Space Station

--- src/logic/data/shipmodels/ShieldSystem.java
+++ src/logic/data/shipmodels/ShieldSystem.java
@@ -1,7 +1,21 @@
 package logic.data.shipmodels;
 
-public class ShieldSystem {
+public class ShieldSystem implements ShipSystem {
     private boolean isAvailable;
+    private boolean isMilitary;
     private int maxShieldsAmount;
     private int shieldsAmount;
+
+    public ShieldSystem(boolean isMilitary) {
+        this.isMilitary = isMilitary;
+
+        if (isMilitary) maxShieldsAmount = 18;
+        else maxShieldsAmount = 9;
+
+        shieldsAmount = maxShieldsAmount;
+    }
+
+    public void replenishArmor() {
+        shieldsAmount = maxShieldsAmount;
+    }
 }
--- src/logic/data/shipmodels/WeaponSystem.java
+++ src/logic/data/shipmodels/WeaponSystem.java
@@ -1,8 +1,22 @@
 package logic.data.shipmodels;
 
-public class WeaponSystem {
+public class WeaponSystem implements ShipSystem {
     private boolean isAvailable;
     private boolean isMilitary;
-    private boolean maxWeapons;
-    private boolean weapons;
+    private int maxWeapons;
+    private int weapons;
+
+    public WeaponSystem(boolean isMilitary) {
+        this.isMilitary = isMilitary;
+
+        maxWeapons = 9;
+        weapons = 9;
+    }
+
+    public void upgradeWeaponSystem() {
+        if (isMilitary && maxWeapons == 18) return;
+        if (!isMilitary) return;
+
+        maxWeapons *= 2;
+    }
 }
