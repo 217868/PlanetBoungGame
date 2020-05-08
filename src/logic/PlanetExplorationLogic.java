d2173fccc2e16b1c419abedb9f9e1e81f57d6b9b
emiliamarkowska <emarkowska10@gmail.com>
2020-05-08 02:59:26 +0200

    resource

--- src/logic/PlanetExplorationLogic.java
+++ src/logic/PlanetExplorationLogic.java
@@ -1,4 +1,12 @@
 package logic;
 
+import logic.data.Resource;
+import logic.data.shipmodels.Drone;
+
+import java.awt.geom.Point2D;
+
 public class PlanetExplorationLogic {
+    Drone drone;
+    Point2D resourcePosition;
+    Resource resource;
 }
